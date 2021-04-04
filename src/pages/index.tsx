846a479c456aff60ef6ff86672d10f993938c6cb
Andrew Debens <andrew.debens@tandem.co.uk>
2021-04-04 22:28:50 +0100

    feat: upgrading to gatsy v3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react"
 
-import styles from './styles.module.scss';
+import { page } from "./styles.module.scss"
 
-const IndexPage: React.FunctionComponent = () => <div className={styles.page} />
+const IndexPage: React.FunctionComponent = () => <div className={page} />
 
 export default IndexPage
