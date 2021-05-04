372a8a0ca404cad0d9fd3df13a247d0a3d59dbbb
Andrew Debens <andrew.debens@tandem.co.uk>
2021-05-04 17:40:09 +0100

    feat: upgrading for the dts-css-modules-loader alpha release

--- src/pages/styles.module.scss.d.ts
+++ src/pages/styles.module.scss.d.ts
@@ -1,12 +1,2 @@
-declare namespace StylesModuleScssNamespace {
-  export interface IStylesModuleScss {
-    page: string
-  }
-}
-
-declare const StylesModuleScssModule: StylesModuleScssNamespace.IStylesModuleScss & {
-  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
-  locals: StylesModuleScssNamespace.IStylesModuleScss
-}
-
-export = StylesModuleScssModule
+export const page: string;
+export const inner: string;
